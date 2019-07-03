f7ed4ee3f3598651e80db44af53bb96c9420f8ad
Davisdavy <davisdvay96@gmail.com>
2019-07-03 09:55:45 +0300

    add a strikethrough event

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -16,8 +16,14 @@ export class GoalComponent implements OnInit {
     new Goal(6, 'Plot my world domination plan', 'Cause I am an evil overlord', new Date(2019, 3, 14)),
   ];
 
-  toggleDetails(i) {
-    this.goals[i].showDescription = !this.goals[i].showDescription;
+  toggleDetails(index) {
+    this.goals[index].showDescription = !this.goals[index].showDescription;
+  }
+
+  completeGoal(isComplete, index) {
+    if (isComplete) {
+      this.goals.splice(index, 1);
+    }
   }
   constructor() { }
 
